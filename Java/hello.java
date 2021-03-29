56cf22fae34c2b00dd60bf3c3d9e9f5d5701b769
Muhammad Saad <63445891+saadjavaid67@users.noreply.github.com>
2021-03-29 13:01:22 +0500

    .

---
+++ Java/hello.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main 
+{
+    public static void main(String []args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello, World");
+        int i=21;
+        String name;
+        System.out.print("What is your name? ");
+        name=sc.nextLine();
+        System.out.println(i+"th is "+name+"'s Fav Date");
+    }
+}
